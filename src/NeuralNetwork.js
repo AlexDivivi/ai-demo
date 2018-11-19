a9e7d6ae56076ed07dbf2513bfb812f10852ade6
Alex <adivivi@gmx.de>
2018-11-19 12:57:39 +0100

    tensorflow prediction in DOM

--- src/NeuralNetwork.js
+++ src/NeuralNetwork.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import * as tf from '@tensorflow/tfjs'
 
 export default class NeuralNetwork extends Component {
+  state = {
+    result: 0
+  }
+
   loadModel() {
     tf.loadModel('data/model/model.json').then(model =>
       this.makePrediction(model)
@@ -10,11 +14,19 @@ export default class NeuralNetwork extends Component {
 
   makePrediction(model) {
     const xs = tf.tensor2d([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
-    const prediction = model.predict(xs, { batchSize: 1 }).print()
-    return prediction
+    const prediction = model.predict(xs, { batchSize: 1 }).flatten()
+    const result = prediction.get([0])
+    this.setState({
+      result: result
+    })
   }
 
   render() {
-    return <div>{this.loadModel()}</div>
+    return (
+      <div>
+        {this.loadModel()}
+        {this.state.result}
+      </div>
+    )
   }
 }
